06e185130bb158e54060572e6a9ac77a7c525bf1
paul-leiva <54649136+paul-leiva@users.noreply.github.com>
2020-08-23 05:21:35 -0400

    Final build ready

--- AutomatedWhatsApp.py
+++ AutomatedWhatsApp.py
@@ -6,24 +6,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from tkinter import *
-#from tkinter.ttk import *
 from time import localtime, strftime
 import time
 import datetime
 
 def schedule_message():
    queue_frame.destroy()
-   q = LabelFrame(window, text = "Queued Messages", height=200, pady=5, padx=5)
    q.grid(row=1, padx=5, sticky=EW)
    recipient_labelq = Label(q, text ='Recipient:')
    time_labelq = Label(q, text ='Will be sent at: ')
-   msg_labelq = Label(q, text ='Message: ')
+   msg_labelq = Label(q, text ='Message: ', justify="left")
    
    recipient_labelq.grid(row = 0, column = 0, padx=5)
    time_labelq.grid(row = 0, column=1)
    msg_labelq.grid(row=0, column=2)
    
-   #time = time.get()
    recipient = contact.get()
    messageq = message.get("1.0", END)
    if (str(time_entry.get()) < strftime("%H:%M", localtime())):
@@ -33,24 +30,17 @@ def schedule_message():
    to_send.append([str(date_q), time_entry.get(), recipient, message.get("1.0", END)])
    to_send.sort()
    print(to_send)
- 
-   # Loop to print messages
-   #Label(q, text=contact.get()).grid(row = 4, column = 0)
-   #Label(q, text=time.get()).grid(row = 4, column = 1)
-   #Label(q, text=message.get("1.0", END), wraplength=500, justify="left").grid(row = 4, column = 2, columnspan=2)
+
+   for x in range(len(to_send)):
+      Label(q, text=to_send[x][2]).grid(row = 4+x, column = 0) # recipient
+      Label(q, text=to_send[x][1]).grid(row = 4+x, column = 1) # time to send
+      Label(q, text=to_send[x][3], wraplength=500, justify="left").grid(row = 4+x, column = 2, columnspan=2) # message
 
 def check_time():
    if (to_send != []):
       if (strftime("%H:%M", localtime()) == to_send[0][1]):
          print(to_send[0][1])
          send_Message()
-   """
-   if (len(to_send) != 0):
-      print(to_send[0][1])
-      if (str(to_send[0][1]) == str(strftime("%H:%M", localtime()))): 
-         to_send.pop(0)
-         print(to_send)
-   """
    window.after(10000, check_time)
    
 def send_Message():
@@ -64,8 +54,6 @@ def send_Message():
       )
    except:
        driver.quit()
-   
-   #time.sleep(25)
    
    user = driver.find_element_by_xpath('//span[@title = "{}" ]'.format(str(to_send[0][2])))
    user.click()
@@ -81,6 +69,31 @@ def send_Message():
    except:
       print('ok')
    driver.quit()
+   print('Your message: ' + to_send[0][3] + ' was sent to ' + to_send[0][2])
+   to_send.pop(0)
+   
+   queue_frame.destroy()
+   q.grid(row=1, padx=5, sticky=EW)
+   recipient_labelq = Label(q, text ='Recipient:')
+   time_labelq = Label(q, text ='Will be sent at: ')
+   msg_labelq = Label(q, text ='Message: ', justify="left")
+   
+   recipient_labelq.grid(row = 0, column = 0, padx=5)
+   time_labelq.grid(row = 0, column=1)
+   msg_labelq.grid(row=0, column=2)
+   
+   recipient = contact.get()
+   messageq = message.get("1.0", END)
+   if (str(time_entry.get()) < strftime("%H:%M", localtime())):
+      date_q = datetime.date.today() + datetime.timedelta(days=1)
+   else:
+      date_q = datetime.date.today()
+   to_send.append([str(date_q), time_entry.get(), recipient, message.get("1.0", END)])
+   
+   for x in range(len(to_send)):
+      Label(q, text=to_send[x][2]).grid(row = 4+x, column = 0) # recipient
+      Label(q, text=to_send[x][1]).grid(row = 4+x, column = 1) # time to send
+      Label(q, text=to_send[x][3], wraplength=500, justify="left").grid(row = 4+x, column = 2, columnspan=2) # message
 
 def toggle_state(*_):
    if(contact.var.get() and time_entry.var.get()):
@@ -91,13 +104,15 @@ def toggle_state(*_):
 current = strftime("%H:%M", localtime())
 print(current)
 
-to_send = [['z', '00:41', 'Nobody', 'Nothing']]#["2020/08/24", "08:08", "ari", "Hello"]] # set of queued messages
+to_send = []
 
 practice = False
 
 window = Tk()
 window.title("AutomatedWhatsApp.py")
 window.geometry("600x600") 
+
+q = LabelFrame(window, text = "Queued Messages", height=200, pady=5, padx=5)
 
 schedule_frame = LabelFrame(window, text = "Schedule a Message", height=200, pady=5, padx = 5)
 queue_frame = LabelFrame(window, text = "Queued Messages", height=200, pady=5, padx=5)
@@ -144,46 +159,3 @@ msg.grid(row=0, column=2)
 check_time()
       
 window.mainloop()
-"""
-while (to_send != []):
-   now = strftime('%H:%M', localtime())
-   #if (time < now):
-   if (str(time_entry) < strftime("%H:%M", localtime())):
-      date = datetime.date.today() + datetime.timedelta(days=1)
-   else:
-      date = datetime.date.today()
-   to_send.append([str(date), time_entry, contact, message])
-   #to_send.sort()
-   print(to_send)
-   print(to_send[0][1])
-   next = to_send[0][1]
-   #if (strftime("%H:%M", localtime()) == to_send[0][1]):
-      #print(to_send[0])
-   #to_send.pop(0)
-"""
-
-
-"""
-def sendMessage():
-   PATH = "C:\Program Files (x86)\chromedriver.exe"
-   driver = webdriver.Chrome(PATH)
-   driver.get('https://web.whatsapp.com/')
-   
-   name = input('Enter name of contact or group: ')
-   message = input('Enter your message: ')
-   
-   input('Enter DONE once you have scanned your QR Code: ')
-   
-   user = driver.find_element_by_xpath('//span[@title = "{}" ]'.format(name))
-   user.click()
-   
-   time.sleep(5)
-   
-   message_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div')
-   time.sleep(5)
-   message_box.send_keys(message)
-   
-   button = driver.find_element_by_class_name('_1U1xa')
-   button.click()
-#main()
-"""
